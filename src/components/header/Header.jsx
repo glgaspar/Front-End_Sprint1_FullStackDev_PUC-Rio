e461bc0a6e3f3cd019dd2a668c19fb8118723402
glgaspar <77644677+glgaspar@users.noreply.github.com>
2023-04-09 18:15:57 -0300

    primeiro modelo de header funcional redirecionamentos funcionando e css horroroso destacando os links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import classes from './Header.module.css'
+import { useNavigate } from "react-router-dom"
 
 export default function Header() {
+
+  // Navigate object and function to change pages based on user clicks
+  const navigate = useNavigate()
+  const navigateRoute = (route) => { 
+    let path = `/${route}`; 
+    navigate(path);
+}
   return (
-    <div>Header</div>
+    <div className={classes.headerContainer}>
+      <div className={classes.headerInfo}>
+        <div className={classes.logoContainer}>
+          <p onClick={()=>navigateRoute('home')}>Logo muito legal</p>
+        </div>  
+        <span className={classes.headerLink} onClick={()=>navigateRoute('sobre')}>Sobre</span>
+        <span className={classes.headerLink} onClick={()=>navigateRoute('contato')}>Contato</span>
+      </div>
+    </div>
   )
 }
