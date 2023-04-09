d2d6cdc13fb097ff6e9c94c429adfa7b8f3e86ec
glgaspar <77644677+glgaspar@users.noreply.github.com>
2023-04-09 17:13:46 -0300

    estrutura básica inicial. provavelmente vai mudar, não consegui decidir ainda o que fazer por enquanto a ideia é montar uma "loja" sem as partes importantes controle de usuários com autenticação vai entrar se de tempo vendas, no máximo colocar um botão q registra no bd, nada sofisticado

---
+++ src/pages/suport/Suport.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+
+export default function Suport() {
+  return (
+    <div>
+      <header><Header /></header>
+      <div>Suport</div>
+    </div>
+  )
+}
