a027bbd26dbb527eb109de9217d8fbab2d64629f
glgaspar <77644677+glgaspar@users.noreply.github.com>
2023-04-09 19:19:06 -0300

    estruturando paginas nas rotas delas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import Contact from './pages/contact/Contact';
+import Product from './pages/product/Product';
+import About from './pages/about/About';
+
+import './index.css'
+
 function App() {
   return (
-    <div>
-    </div>
+    <Router>      
+      <header>
+        <Header />
+      </header>
+      <main>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/produto' element={<Product />} />
+            <Route path='/contato' element={<Contact />} />
+            <Route path='/sobre' element={<About />} />
+          </Routes>
+      </main>
+    </Router>      
   );
 }
 
