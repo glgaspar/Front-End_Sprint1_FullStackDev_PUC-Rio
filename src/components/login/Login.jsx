0876715fce8fa0b2d98f0330f971535306b2b113
glgaspar <77644677+glgaspar@users.noreply.github.com>
2023-04-22 13:05:21 -0300

    login form workin. Styling still not good

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import classes from "./login.module.css";
+import { APIPost } from "../api/Api";
+import NewUserForm from "./NewUserForm";
+
+export default function Login() {
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		if (event.target.className.includes("loginForm")) {
+			const data = {
+				email: event.target.email.value,
+				pssw: event.target.password.value,
+			};
+			console.log(data);
+			APIPost("/login", data).then((data) => {
+				console.log(data.data);
+				if (data.data.message.message === "Login successful") {
+					sessionStorage.setItem("sprint1", JSON.stringify(data.data.user));
+					window.location.reload();
+				} else {
+					alert("Usuário não encontrado");
+				}
+			});
+		}
+	};
+
+	return (
+		<div className={classes.loginContainer}>
+			<form className={classes.loginForm} onSubmit={(e) => handleSubmit(e)}>
+				<h3>Entrar na sua conta</h3>
+				<div className={classes.formGroup}>
+					<label htmlFor="email">E-mail</label>
+					<input type="email" name="email" required></input>
+				</div>
+				<div className={classes.formGroup}>
+					<label htmlFor="password">Senha</label>
+					<input type="password" name="password" required></input>
+				</div>
+				<button type="submit">Entrar</button>
+				<NewUserForm />
+			</form>
+		</div>
+	);
+}
