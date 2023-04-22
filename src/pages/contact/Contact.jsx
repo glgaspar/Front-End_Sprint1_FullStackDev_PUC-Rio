c69efc2168d4cebd36e590c8c1c8bfad0f7e3f71
glgaspar <77644677+glgaspar@users.noreply.github.com>
2023-04-22 13:10:22 -0300

    fixed naming on contact form and added post

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import classes from './Contact.module.css'
 import SimpleButton from '../../components/button/SimpleButton'
+import {APIPost} from '../../components/api/Api'
 
 export default function Contact() {
 
   const handleSubmit = (event)=>{
     event.preventDefault()
     console.log(event)
+    const data = {
+      name:event.target.nome.value,
+      email:event.target.email.value,
+      tel:event.target.telefone.value,
+      message:event.target.mensagem.value
+    }
+    console.log(data)
+    APIPost('/contact', data)
   }
   
   return (
@@ -22,12 +31,12 @@ export default function Contact() {
 
         <div className={classes.formGroup}>
           <label htmlFor='email'>E-mail</label>
-          <input name='email'></input>
+          <input type='email' name='email' required></input>
         </div>
 
         <div className={classes.formGroup}>
           <label htmlFor='telefone'>Telefone</label>
-          <input name='telefone'></input>
+          <input type='tel' name='telefone'></input>
         </div>
 
         <div className={classes.formGroup}>
